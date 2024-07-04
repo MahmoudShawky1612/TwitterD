afbdcfd4ca4d63f94ebc6e565657ffc0c000aa03
MahmoudShawky1612 <nostyy30@gmail.com>
2024-07-04 08:19:19 +0300

    image picker

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fpdart/fpdart.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackBar(BuildContext context, String content){
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(content)));
@@ -8,4 +10,16 @@ void showSnackBar(BuildContext context, String content){
 
 String getNameFromEMail(String email){
   return email.split('@')[0];
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  final ImagePicker picker = ImagePicker();
+  final imageFiles = await picker.pickMultiImage();
+  if (imageFiles.isNotEmpty) {
+    for (final image in imageFiles) {
+      images.add(File(image.path));
+    }
+  }
+  return images;
 }
--- lib/tweet/view/create_tweet_view.dart
+++ lib/tweet/view/create_tweet_view.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitterclone/common/common.dart';
+import 'package:twitterclone/constants/assets_constants.dart';
+import 'package:twitterclone/core/utils.dart';
 import 'package:twitterclone/features/auth/controller/auth_controller.dart';
 import 'package:twitterclone/theme/pallete.dart';
+import 'dart:io';
 
 import '../../common/rounded_small_button.dart';
 
@@ -16,14 +21,29 @@ class CreateTweetScreen extends ConsumerStatefulWidget {
 }
 
 class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
+  final tweetTextController=TextEditingController();
+   List<File> images=[];
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    tweetTextController.dispose();
+  }
+
+  void onPickImages()async{
+    images=await pickImages();
+    setState(() {});
+  }
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDetailsProvider).value;
     return Scaffold(
       appBar:AppBar(
         leading:IconButton(
-          onPressed:(){},
-          icon:Icon(Icons.close,size:30) ,
+          onPressed:(){
+            Navigator.pop(context);
+          },
+          icon:Icon(Icons.close,size:30 ) ,
         ),
         actions: [
           RoundedSmallButton(onTap:(){},label:"Tweet",backgroundColor: Pallete.blueColor,textColor: Pallete.whiteColor,),
@@ -39,11 +59,56 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
                      backgroundImage: NetworkImage(currentUser.profilePic ),
                      radius: 20,
                 ),
+                    SizedBox(width: 15,),
+                    Expanded(
+                      child: TextField(
+                        controller: tweetTextController,
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "What's happening ?",
+                          hintStyle: TextStyle(
+                            color: Pallete.greyColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600
+                          ),
+                          border: InputBorder.none,
+                        ),
+                        maxLines: null,
+                      ),
+                    )
               ],
             ),
-                    ],
+              ],
                   ),
-          )),
+          )
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top:BorderSide(
+              width: .3,
+              color:Pallete.greyColor,
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0).copyWith(bottom: 10,left: 10,right: 10),
+            child: GestureDetector(
+                onTap:onPickImages,
+                child: SvgPicture.asset(AssetsConstants.galleryIcon)),
+          ),Padding(
+            padding: const EdgeInsets.all(8.0).copyWith(bottom: 10,left: 10,right: 10),
+            child: SvgPicture.asset(AssetsConstants.gifIcon),
+          ),Padding(
+            padding: const EdgeInsets.all(8.0).copyWith(bottom: 10,left: 10,right: 10),
+            child: SvgPicture.asset(AssetsConstants.emojiIcon),
+          ),
+        ],),
+      ),
     );
   }
 }
